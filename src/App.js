ddf05e08d6be5d75dfee54fc0a78066cc3c85758
qizongliang <72285030+qizongliang@users.noreply.github.com>
2023-09-16 02:40:47 -0400

    Nav bar. Not responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './NavBar'
+import { Route, Routes } from 'react-router'
+import Prices from './pages/Prices/Prices'
+import Products from './pages/Products/Products'
+import Shoppingcart from './pages/ShoppingCart/ShoppingCart'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/Prices" element={<Prices />}></Route>
+        <Route path="/Products" element={<Products />}></Route>
+        <Route path="/Shoppingcart" element={<Shoppingcart />}></Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+const products = () => {
+  return (
+    <div>
+      <Typography variant="h4" align="center" sx={{ mx: 20, mt: 5 }}>
+        Products
+      </Typography>
+    </div>
+  )
+}
+
+export default products
